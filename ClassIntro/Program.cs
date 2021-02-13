2110dd9c68cb6b6ef9e3feefe17b8d52a6887e24
oguzhanyazar1 <oguzhan.yazar.fenerbahce@gmail.com>
2021-02-14 00:17:23 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs kurs1 = new Kurs();
+            kurs1.kursAdi = "C#";
+            kurs1.Egitmen = "Engin Demirog";
+            kurs1.IzlenmeOranı = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.kursAdi = "Java";
+            kurs2.Egitmen = "Kerem Varış";
+            kurs2.IzlenmeOranı = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.kursAdi = "Python";
+            kurs3.Egitmen = "Berkay Bilgin";
+            kurs3.IzlenmeOranı = 80;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.kursAdi = "C++";
+            kurs4.Egitmen = "Murat Kurtboğan";
+            kurs4.IzlenmeOranı = 100;
+
+            //Console.WriteLine(kurs1.kursAdi + " : " + kurs1.Egitmen);
+
+            Kurs[] kurslar = new Kurs[] {kurs1,kurs2,kurs3,kurs4 };
+
+            foreach (Kurs kurs in kurslar)
+            {
+                Console.WriteLine(kurs.kursAdi + " : " + kurs.Egitmen);
+            }
+
+        }
+    }
+
+    class Kurs
+    {
+        public string kursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOranı { get; set; }
+    }
+
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 9;
+            while (a < 10)
+            {
+                Console.WriteLine("SEVMEK");
+            }
+        }
+    }
+}
